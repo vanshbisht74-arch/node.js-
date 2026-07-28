81c2a8bee8e170f0d00c572e22444952781b3c61
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-07-29 00:46:25 +0530

    Add files via upload

---
+++ 2import.js
@@ -0,0 +1,4 @@
+const math=require('./2.js')
+
+console.log('Addition Result',math.add(2,3))
+console.log('Substraction Result',math.sub(5,9))
---
+++ app.js
@@ -0,0 +1,3 @@
+// NODEJS:-It is a enviroment to run the js outside the browser built on the chrome's 
+// v8 engine.
+
