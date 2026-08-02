d557966f62f21cd73bc0b28623549952eeb5d78b
Vansh Viraj Bisht <vanshbisht74@gmail.com>
2026-08-02 23:56:29 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,37 @@
+const fs=require("fs")
+
+// Writing the file
+// sync 
+
+fs.writeFileSync('./text.txt','Hello world Sync')
+
+//async
+fs.writeFile('./text.txt','Hello world Async',(err)=>{});
+
+// The async always need a callback fn as it cannot return the result like the sync .
+
+// Writing the File:-
+//sync:-
+
+const readsync=fs.readFileSync("./text.txt",'utf-8')
+console.log(readsync)
+//async:-The async do not return the result it take a callback to return.
+
+fs.readFile('./text.txt','utf-8',(err,result)=>{
+
+    if(err){
+        console.log(err)
+    }
+    else
+    {
+       console.log(result)
+    }
+});
+// //Appending:-
+fs.appendFileSync('./text.txt','Hey There')
+
+
+// //Deleting the File:-
+fs.unlinkSync('./delete.txt');
+
+
